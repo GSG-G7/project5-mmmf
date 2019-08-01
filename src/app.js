9d3135b222fe7e780d906a29d109d40d55f56a19
mossa-Sammer <mossa708496@gmail.com>
2019-08-01 10:10:43 +0300

    refactoring done relates #24

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 /* eslint-disable import/no-unresolved */
-const express = require('express');
 const { join } = require('path');
+const express = require('express');
 const compression = require('compression');
-const controller = require('./controller');
+
+const router = require('./controller');
 
 const app = express();
 
-app.disable('x-powerd-by');
 app.set('port', process.env.PORT || 3000);
+
+app.disable('x-powerd-by');
+
 app.use(compression());
+app.use(express.static(join(__dirname, '..', 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(join(__dirname, '..', 'public')));
-app.use(controller);
+app.use(router);
 
 module.exports = app;
