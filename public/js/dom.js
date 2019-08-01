66a85de21604e6ad7e3a703e57371f3971ea355f
fadi omar <fadeomar2015@gmail.com>
2019-08-01 10:33:42 +0300

    follow linter roules and handel error Realtes #23 #25

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-mixed-spaces-and-tabs */
-/* eslint-disable no-tabs */
-// import { promises } from "dns";
 
 const input = document.querySelector('.input-feild');
 
@@ -14,27 +11,22 @@ btn.addEventListener('click', () => {
     method: 'POST',
     body: word,
     headers: { 'Content-Type': 'application/json' },
-  })
-    .then(res => res.json())
-    .then((res) => {
-      console.log('results', res);
-      return res.results;
-    })
+  }).then(res => res.json())
+    .then(res => res.results)
     .then(res => res.forEach((e) => {
-	  console.log(e.title);
-	  const poster = document.createElement('div');
-	  const posterImg = document.createElement('img');
-	  const posterTitle = document.createElement('h2');
-	  const posterOverview = document.createElement('P');
-	  poster.classList.add('poster');
-	  posterImg.classList.add('posterImg');
-	  posterTitle.classList.add('posterTitle');
-	  posterImg.setAttribute('src', `https://image.tmdb.org/t/p/w500/${e.poster_path}`);
-	  poster.appendChild(posterTitle);
-	  poster.appendChild(posterImg);
-	  posterTitle.textContent = e.title;
-	  posterOverview.textContent = e.overview;
-	  poster.appendChild(posterOverview);
-	  result.appendChild(poster);
+      const poster = document.createElement('div');
+      const posterImg = document.createElement('img');
+      const posterTitle = document.createElement('h2');
+      const posterOverview = document.createElement('P');
+      poster.classList.add('poster');
+      posterImg.classList.add('posterImg');
+      posterTitle.classList.add('posterTitle');
+      posterImg.setAttribute('src', `https://image.tmdb.org/t/p/w500/${e.poster_path}`);
+      poster.appendChild(posterTitle);
+      poster.appendChild(posterImg);
+      posterTitle.textContent = e.title;
+      posterOverview.textContent = e.overview;
+      poster.appendChild(posterOverview);
+      result.appendChild(poster);
     }));
 });
